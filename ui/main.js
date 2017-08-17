9bd1c0e3e3053ce1985329581de4c2a3eab92ae7
Vamshi1104 <vamshik408@gmail.com>
2017-08-17 20:46:16 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,6 +2,13 @@ console.log('Loaded!');
 var element=document.getElementById('main-text');
 element.innerHTML="vamshi";
 var element=document.getElementById('madi');
+var marginLeft=0;
+function moveRight(){
+    marginLeft=marginLeft+10;
+    madi.style.marginLeft=marginLeft + 'px';
+}
+
 madi.onclick=function(){
-    madi.style.marginLeft='100px';
+   var interval=setInterval(moveRight,100);
+  
 };
