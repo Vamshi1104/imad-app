a7891906375e5795db61779a986577cfa0ef0118
Vamshi1104 <vamshik408@gmail.com>
2017-08-22 19:50:46 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var Pool= require('pg').Pool;
 var config={
     user:'vamshik408',
     database:'vamshik408',
-    host:'db.imad.hasura-io',
+    host:'db.imad.hasura-app.io',
     port:'5432',
     password:process.env.DB_PASSWORD
 };
